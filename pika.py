ceb303cea254745865c15e19d9863ac864a779bb
Drake343 <87871563+Drake343@users.noreply.github.com>
2021-12-11 16:45:34 +0530

    Voice Assistant : created

--- pika.py
+++ pika.py
@@ -1,5 +1,10 @@
 import pyttsx3
 import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,8 +26,79 @@ def wishMe():
 
         else:
                 speak("Good Evening!")
-        speak("I am Pika Sir.Please tell me how may I help you")
- 
 
+        speak("I am Pika Sir.Please tell me how may I help you")
+
+def takeCammand():
+       
+
+        r=sr.Recognizer()
+        with sr.Microphone() as source:
+                print("Listening...")
+                r.pause_threshold=1
+                audio=r.listen(source)
+        try:
+                print("Recognizing...")
+                query = r.recognize_google(audio,language='en-in')
+                print(f"user said:{query}\n")
+
+        except Exception as e:
+                print(e)
+
+                print("Say that again please...")
+                return "None"
+
+        return query
+
+def sendEmail(to,content):
+  server =smtplib.SMTP('smntp.gmail.com',587)
+  server.ehlo()
+  server.starttls()
+  server.login('youremail.com','your_password')
+  server.sendmail('youremail@gmail.com',to,content)
+  server.close()
 
 wishMe()
+# while True:
+if 1:
+    query=takeCammand().lower()
+
+    #logic for executing tasks based on query
+    if 'wikipedia' in query:
+        speak("search Wikipedia...")
+        query=query.replace("wikipedia","")
+        results =wikipedia.summary(query,sentences=2)
+        speak("According to Wikipedia")
+        print(results)
+        speak(results)
+
+    elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+    elif 'open google' in query:
+            webbrowser.open("google.com")
+
+    elif 'open stack overflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+    elif 'play music' in query:
+            music_dir='E:\\songs\\j'
+            songs =os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir,songs[0]))
+
+    elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+    
+    
+    elif 'email to drake' in query:
+            try:
+                speak("What should I say?")
+                content = takeCammand()
+                to = "harryyourEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend harry bhai. I am not able to send this email") 
